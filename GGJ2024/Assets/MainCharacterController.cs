f7d20fe095e4eb6a0f1e712e6a5511e575ba2358
Phil <ptwalsh7@gmail.com>
2024-01-23 21:45:23 -0800

    minor script changes

--- GGJ2024/Assets/MainCharacterController.cs
+++ GGJ2024/Assets/MainCharacterController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class MainCharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,6 +16,6 @@ public class MainCharacterController : MonoBehaviour
     {
         var x = Input.GetAxis("Horizontal");
         var y = Input.GetAxis("Vertical");
-        GetComponent<Rigidbody2D>().AddForce(3 * new Vector2(x, y));
+        rb.AddForce(3 * new Vector2(x, y));
     }
 }
